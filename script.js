3b40cef1a25e53a005a08d916aa807078700366a
Daniel Brenes Fonseca <danibrenes04@gmail.com>
2023-08-04 01:51:47 -0600

    Test form #1

--- script.js
+++ script.js
@@ -6,11 +6,53 @@ function toggleScrollToTopButton() {
         scrollToTopButton.style.opacity = 0;
     }
 }
-
-// Function to scroll to the top of the page
 function scrollToTop() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
 }
-
-// Event listener to check the scroll position and toggle the button visibility
 window.addEventListener('scroll', toggleScrollToTopButton);
+
+document.addEventListener('DOMContentLoaded', function () {
+    
+    const downloadCWI = document.getElementById('downloadCWI');
+    downloadCWI.addEventListener('click', function () {
+        const downloadlinkCWI = document.getElementById('downloadlinkCWI');
+        downloadlinkCWI.click();
+    });
+
+    const downloadCWE = document.getElementById('downloadCWE');
+    downloadCWE.addEventListener('click', function () {
+        const downloadlinkCWE = document.getElementById('downloadlinkCWE');
+        downloadlinkCWE.click();
+    });
+        const downloadCVI = document.getElementById('downloadCVI');
+    downloadCVI.addEventListener('click', function () {
+        const downloadlinkCVI = document.getElementById('downloadlinkCVI');
+        downloadlinkCVI.click();
+    });
+
+
+});
+
+const FormularioContacto = document.getElementById('Formulario-Contacto');
+
+FormularioContacto.addEventListener('enviar',function (event) {
+    event.preventDefault()
+
+    const datosForm = new  FormData(FormularioContacto);
+
+    datosForm.append('subject', 'Contacto desde formulario');
+
+    fetch('/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: new URLSearchParams(datosForm).toString(),
+    })
+    .then((response) => {
+        console.log('Formulario enviado');
+    })
+    .catch((error) => {
+        console.error('Error al enviar el formulario:', error);
+    });
+});
